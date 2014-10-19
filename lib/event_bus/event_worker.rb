287e7b77ceffa8967d90b287b8f4b12a39c90edb
Mark Sargent <sargent.mark@gmail.com>
2014-10-19 17:22:35 +1300

    Removed settings, these are already the defaults

--- lib/event_bus/event_worker.rb
+++ lib/event_bus/event_worker.rb
@@ -4,8 +4,6 @@ require_relative 'registrations'
 class EventWorker
   include Sidekiq::Worker
 
-  sidekiq_options({ retry: true, failures: true })
-
   def perform(event_name, payload)
     # p event_name, payload
     EventBus::Registrations.instance.announce(event_name, payload, true)
